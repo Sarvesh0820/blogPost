ce363badf472cca98c1ad1e5705cac2996010295
sarvesh0820 <sarveshhadgaonkar01@gmail.com>
2024-08-05 09:51:50 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Select from "./Select"
+import Logo from "./Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
+
+
+export{
+    Header,
+    Footer,
+    Logo,
+    LogoutBtn,
+    Container,
+    Button,
+    Input,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Select
+
+}
